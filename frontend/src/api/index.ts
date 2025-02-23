96c905dea0af745e7c780c2538d353af7aaf4ca9
AAleksandros <aleksandrosa20@gmail.com>
2025-02-23 18:39:28 +0200

    Minor security change

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL:"https://4ce8-2a02-2149-8adf-6f00-d635-2576-893e-9c49.ngrok-free.app/api", // CHANGE
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true"
   },
 });
 
@@ -15,5 +16,22 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// Handle unauthorized errors (401) by clearing storage and redirecting to login
+api.interceptors.response.use(
+  response => response, 
+  error => {
+    if (error.response?.status === 401) {
+      console.warn("Unauthorized request - clearing session data.");
+
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("user");
+      localStorage.removeItem("profile");
+
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
